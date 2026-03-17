ef4ea900e040de0ed66278bc5c6c88118ee4d4fc
wenda.sheng <wenda.sheng@ytever.com>
2026-03-17 10:43:19 +0800

    删除不必要的文件

---
+++ .runtime/package_cache/algo_missing_value/1.0.0/build/project/algo_missing_value/meta.py
@@ -0,0 +1,12 @@
+"""缺失值处理算法元数据。"""
+
+from algorithm_sdk import AlgorithmMeta, ExecutionMode
+
+ALGORITHM_META = AlgorithmMeta(
+    algo_code="missing_value",
+    name="缺失值处理",
+    category="data_cleaning",
+    version="1.0.0",
+    execution_mode=ExecutionMode.IN_MEMORY,
+    description="将空值替换为指定默认值。",
+)
